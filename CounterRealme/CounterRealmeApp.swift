4c914d22a2d7ccae3a5349f9608515a967e4f05e
sakurakotubaki <gitonly543@gmail.com>
2023-11-25 19:32:32 +0900

    Initial Commit

---
+++ CounterRealme/CounterRealmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterRealmeApp.swift
+//  CounterRealme
+//
+//  Created by 橋本純一 on 2023/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CounterRealmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
